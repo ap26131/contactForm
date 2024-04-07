c801e21e84d01a8f01cbdd626d65acee3f83439f
ap26131 <ap26131@georgiasouthern.edu>
2024-04-07 19:21:10 -0400

    Uploaded php file

---
+++ thankyou_page.php
@@ -0,0 +1,13 @@
+<html><head><title>Thank you</title></head>
+    <body>
+        <h1>Thank you for Registration!</h1>
+        <p>Your information is:</p>
+        <p>First Name: <?php echo $_POST[‘fnamee’] ?>
+        <p>Last Name: <?php echo $_POST[‘lname’] ?>
+        <p>Address Name: <?php echo $_POST[‘address’] ?>
+        <p>State: <?php echo $_POST[‘state’] ?>
+        <p>Zip: <?php echo $_POST[‘zip’] ?>
+        <p>Telephone: <?php echo $_POST[‘tphone’] ?>
+        <p>Email: <?php echo $_POST[‘email’] ?>
+    </body>
+</html>
