6f395b544d95f5f97a17f776f4a2b1c425e1402c
ap26131 <ap26131@georgiasouthern.edu>
2024-04-07 21:14:35 -0400

    Updated files from server

--- thankyou_page.php
+++ thankyou_page.php
@@ -1,13 +1,19 @@
-<html><head><title>Thank you</title></head>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<title>Thank you</title>
+</head>
     <body>
-        <h1>Thank you for Registration!</h1>
-        <p>Your information is:</p>
-        <p>First Name: <?php echo $_POST[‘fname’] ?>
-        <p>Last Name: <?php echo $_POST[‘lname’] ?>
-        <p>Address Name: <?php echo $_POST[‘address’] ?>
-        <p>State: <?php echo $_POST[‘state’] ?>
-        <p>Zip: <?php echo $_POST[‘zip’] ?>
-        <p>Telephone: <?php echo $_POST[‘tphone’] ?>
-        <p>Email: <?php echo $_POST[‘email’] ?>
+		<div class="container"> 
+		<h1>Thank you for Registration!</h1>
+        <h4>Your information is:</h4>
+        <p>First Name: <?php echo $_POST['fname']?></p>
+        <p>Last Name: <?php echo $_POST['lname']?></p>
+        <p>Address Name: <?php echo $_POST['address']?></p>
+        <p>State: <?php echo $_POST['state']?></p>
+        <p>Zip: <?php echo $_POST['zip']?></p>
+        <p>Telephone: <?php echo $_POST['tphone']?></p>
+        <p>Email: <?php echo $_POST['email']?></p>
+		</div>
     </body>
 </html>
